d86ea190c0df34884b8561c889259264557ee637
Viktoriia <shevictory@gmail.com>
2022-04-10 16:51:42 +0300

    Add prop children: NamedList

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
-import LoginForm from './components/LoginForm';
+import React from 'react';
 
-const App = () => <LoginForm />;
+const ListItem = props => {
+  const { children } = props;
+  return <div>{children}</div>;
+};
+
+// const App = () => <ListItem title='title' children='I am Grut' />;
+
+// реализовать NamedList: именованный список с заголовком из name
+// и содержимым списка из контента NamedList
+
+const NamedList = props => {
+  const { name, children } = props;
+  return (
+    <div>
+      <h2>{name}</h2>
+      <ul>{children}</ul>
+    </div>
+  );
+};
+
+const App = () => (
+  <NamedList name='Odd numbers'>
+    <li>1</li>
+    <li>3</li>
+    <li>5</li>
+  </NamedList>
+);
+
+// const App = () => <ListItem title='title'>I am Grut</ListItem>;
+// React.createElement(ListItem, { title: 'title' }, 'I am Grut');
 
 export default App;
