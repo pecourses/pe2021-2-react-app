4dce657d384bc367bb18d5dc11de9282fe0df008
Viktoriia <shevictory@gmail.com>
2022-04-17 16:01:14 +0300

    Add useEffect ex

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import { useState } from 'react';
 import ClickerCounter from './components/ClickerCounter';
-import Counter from './components/CounterH';
 
-const App = () => <Counter />;
+const App = () => {
+  const [isVisible, setIsVisible] = useState(true);
+
+  return (
+    <>
+      <button onClick={() => setIsVisible(isVisible => !isVisible)}>
+        {isVisible ? 'Hide' : 'Show'}
+      </button>
+      {isVisible && <ClickerCounter />}
+    </>
+  );
+};
 
 export default App;
--- src/components/ClickerCounter/index.js
+++ src/components/ClickerCounter/index.js
@@ -1,32 +1,65 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function ClickerCounter () {
-  // this.state = {
-  //   count: 0,
-  // };
   const [count, setCount] = useState(0);
 
-  const [coords, setCoords] = useState({ x: 0, y: 0 });
-
-  // this.setState({ count: count + 1 });
   const clickHandler = () => setCount(count => count + 1);
 
-  const mouseMoveHandler = e => {
-    setCoords({ x: e.clientX, y: e.clientY });
-  };
+  // componentDidMount() {
+  //    document.title = `${count}`;
+  //    document.body.addEventListener('click', clickHandler);
+  // }
+  //  componentDidUpdate() {
+  //   document.title = `${count}`;
+  // }
+  // componentWillUnmount() {
+  //   document.body.removeEventListener('click', clickHandler);
+  // }
+
+  useEffect(() => {
+    // после каждого рендера (cdm+cdu)
+    document.title = `${count}`;
+  });
+
+  useEffect(() => {
+    document.body.addEventListener('click', clickHandler);
+    return () => {
+      document.body.removeEventListener('click', clickHandler);
+    };
+  });
+
+  useEffect(() => {
+    console.log('useEffect', count);
+    return () => {
+      console.log('useEffect reset', count);
+    };
+  }, [count]);
 
   return (
-    <div
-      style={{ height: '100vh' }}
-      onClick={clickHandler}
-      onMouseMove={mouseMoveHandler}
-    >
+    <div style={{ height: '100vh' }}>
+      <p>{console.log('render')}</p>
       <p>Count: {count}</p>
-      <p>
-        Coords: {coords.x} {coords.x}
-      </p>
     </div>
   );
 }
 
 export default ClickerCounter;
+
+// useEffect(() => {
+//   // Побочный эффект после каждого рендера (cdm+cdu)
+//   return () => {
+//     // Сброс побочного эффекта перед следующим рендером
+//     // (+ перед размонтированием)
+//   };
+// },[список зависимостей]);
+
+// Если список зависимостей пустой ([]), то cdm+cwu
+
+// Побочный эффект:
+// - не требуют сброса:
+// --- обращение к DOM
+// --- подгрузка данных
+// --- логгирование
+// - требующие сброса:
+// --- навешивание обработчиков
+// --- таймауты/интервалы
