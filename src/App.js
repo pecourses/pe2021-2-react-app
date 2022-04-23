8711f5f8bb5c201d2feeb20a8ca552b45709bc16
Viktoriia <shevictory@gmail.com>
2022-04-23 20:32:33 +0300

    Add yup validation schema

--- src/App.js
+++ src/App.js
@@ -1,20 +1,5 @@
-import { useState } from 'react';
-import { UserContext } from './contexts';
-import UserPage from './pages/UserPage';
+import UserInfoForm from './components/UserInfoForm';
 
-const App = () => {
-  const [user, setUser] = useState({
-    fN: 'Test',
-    lN: 'Testovich',
-    src:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dXNlciUyMHByb2ZpbGV8ZW58MHx8MHx8&w=1000&q=80',
-  });
-
-  return (
-    <UserContext.Provider value={user}>
-      <UserPage />
-    </UserContext.Provider>
-  );
-};
+const App = () => <UserInfoForm />;
 
 export default App;
---
+++ src/utils/validationSchemas.js
@@ -0,0 +1,38 @@
+import * as yup from 'yup';
+
+export const USER_SCHEMA = yup.object({
+  firstName: yup
+    .string()
+    .trim()
+    .min(2)
+    .max(60)
+    .required(),
+  age: yup
+    .number()
+    .min(0)
+    .max(130)
+    .integer()
+    .required(),
+});
+
+// const user = { firstName: 'Test', age: 65 };
+
+// 1 schema.isValid(Sync) => true/false
+// const isUserValid = USER_SCHEMA.isValidSync(user);
+// console.log('isUserValid', isUserValid);
+
+// 2 schema.validate(Sync) => value/error
+// try {
+//   const validatingUser = USER_SCHEMA.validateSync(user);
+//   console.log('validatingUser', validatingUser);
+// } catch (e) {
+//   console.error(e);
+// }
+
+// USER_SCHEMA.validate(user)
+//   .then(validatingUser =>
+//     console.log('validatingUser from then', validatingUser)
+//   )
+//   .catch(e => console.error(e));
+
+// console.log('final');
